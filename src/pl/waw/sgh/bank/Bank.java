6db787d77f289fcb38868566cf4fcb91560d6124
slynny <slynnywik@gmail.com>
2018-05-16 09:15:42 +0200

    aktualizacja kodów 16.05

--- src/pl/waw/sgh/bank/Bank.java
+++ src/pl/waw/sgh/bank/Bank.java
@@ -1,8 +1,10 @@
 package pl.waw.sgh.bank;
 
-        import java.math.BigDecimal;
-        import java.util.ArrayList;
-        import java.util.List;
+import pl.waw.sgh.bank.exceptions.InvalidAmountException;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bank {
 
@@ -10,22 +12,13 @@ public class Bank {
 
     List<Account> accList = new ArrayList<>();
 
-
-
-
-    //this is called a sequence in databases
-    // -- storing the last ID
-
     private Integer lastCustID = 0;
 
     private Integer lastAccID = 0;
 
-    //factory pattern
-    //defining, calling a method that ....
-
     public Customer createCustomer(String firstName,
                                    String lastName,
-                                   String email) { //reference to a customer
+                                   String email) {
         Customer customer = new Customer(lastCustID++,firstName, lastName, email);
         custList.add(customer);
         return customer;
@@ -41,12 +34,42 @@ public class Bank {
         return account;
     }
 
+    public void transfer(Integer fromAccId, Integer toAccId, double amount) throws InvalidAmountException{
+        Account fromAcc = findAccountById(fromAccId);
+        Account toAcc = findAccountById(toAccId);
+        fromAcc.charge(amount);
+        toAcc.deposit(amount);
+    }
+/*
+    public void transfer(Integer fromAccId, Integer toAccId, double amount) {
+        Account fromAcc = findAccountById(fromAccId);
+        Account toAcc = findAccountById(toAccId);
+        int res = fromAcc.charge(amount);
+        if (res<0) {
+            System.out.println("Not enough money");
+            System.exit(res);
+        }
+        toAcc.deposit(amount);
+    }
+*/
+
+    public Account findAccountById(int id) {
+        // iterate through the account list and return the account with a given id
+        for (Account acc : accList) {
+            //if (id.equals(acc.getAccountID()))
+            if (id==acc.getAccountID())
+                return acc;
+        }
+
+        return null;
+    }
+
+
     @Override
     public String toString() {
         return "Bank{" +
-                "custList=" + custList +
-                ", accList=" + accList +
+                "cust=" + custList +
+                "\nacc=" + accList +
                 '}';
     }
 }
-
